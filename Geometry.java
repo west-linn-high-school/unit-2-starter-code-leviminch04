f4a53669f7457abc889b6ec2efbefa99293d9ca5
Michael George <levi.minch@gmail.com>
2019-09-10 09:33:04 -0700

    math stuff

--- Geometry.java
+++ Geometry.java
@@ -2,11 +2,17 @@ public class Geometry{
     public static void main(String[] args){
         int Sides = 7;
         int Radius = 5;
-        final int PI = 22/7;
+        final double PI = 3.1415;
         System.out.println("A heptagon has " + Sides + " sides.");
-        System.out.println("A decagon has " + Radius + " sides.");
+        System.out.println("A decagon has " + (Sides + 3) + " sides.");
         System.out.println("A dodecagon has " + (Sides + Radius) + " sides.");
-        System.out.println("The circumference of a circle with a radius of " + Radius + " is " + ((Radius * 2) * PI));
-        
+        System.out.println("The circumference of a circle with a radius of " + Radius + " is " + ((Radius * 2) * PI + "."));
+        int c; 
+        int a = 40;
+        double y = 6.0;
+        int b = 6;
+        double z;
+        z = (double) a/b;
+        System.out.println("z= " + z);
     }
 }
