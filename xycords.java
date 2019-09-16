a91aaa6b65f1b04e67f2ab5cd86415018f347c34
Levi Minch <levi@Levis-MacBook-Pro.local>
2019-09-16 16:26:54 -0700

    xyCords

--- xycords.java
+++ xycords.java
@@ -1,20 +1,28 @@
 import java.util.Scanner;
-import java.text.DecimalFormat;
 
 public class xyCords{
   public static void main(String[] args){
     Scanner scan = new Scanner(System.in);
 
-    System.out.println("enter your X coordinate");
-    String xCordStr = scan.next();
-    Integer xCord = Integer.valueOf(xCordStr);
+    System.out.println("Enter your X coordinate of your first set");
+    Double xCord1 = scan.nextDouble();
 
-    System.out.println("Enter your Y coordinate");
-    String yCordStr = scan.next();
-    Integer yCord = Integer.valueOf(yCordStr);
+    System.out.println("Enter your Y coordinate of your first set");
+    Double yCord1 = scan.nextDouble();
+
+    System.out.println("Enter your X coordinate of your second set");
+    Double xCord2 = scan.nextDouble();
+
+    System.out.println("Enter your Y coordinate of your second set");
+    Double yCord2 = scan.nextDouble();
 
     System.out.println();
-    System.out.println("Your coordinates are: (" + xCord + "," + yCord + ")");
-    System.out.println("The distance between your coordinates is: " + Math.abs(xCord - yCord));
+    double xnum = (xCord1 - xCord2);
+    Double xSquare = Math.pow(xnum, 2);
+    double ynum = (yCord1 - yCord2);
+    Double ySquare = Math.pow(ynum, 2);
+    Double xySquare = (ySquare + xSquare);
+    Double difference = Math.sqrt(xySquare);
+    System.out.println(difference);
   }
 }
