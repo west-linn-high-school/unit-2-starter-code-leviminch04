6ece701fb139f3f9b591f9b351a2942a13c8d6a3
Levi Minch <levi.minch@gmail.com>
2019-09-17 09:52:12 -0700

    recipt
    recipt

---
+++ Recipt.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Receipt{
+  public static void main(String[] Args){
+    Scanner scan = new Scanner(System.in);
+    //print out name of store
+    System.out.println("Steve’s S-Mart");
+    //name of cashier
+    System.out.println("Name of Cashier: ");
+    String cashierName = scan.next();
+    //time and date
+    String dateTime = "9/17/2019 9:48 AM";
+    System.out.println("The time and Date are: " + dateTime);
+    System.out.println();
+    //getting amount of books, movies and peanuts
+    System.out.println("Amount of books");
+    double books = scan.nextDouble();
+    System.out.println("Amount of movies")
+    double movies = scan.nextDouble();
+
+  }
+}
